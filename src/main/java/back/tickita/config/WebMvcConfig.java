7d34dc9f32d7f07fc29c9c7514b421dc0767b764
lby9906 <lby9906@naver.com>
2024-06-12 17:56:16 +0900

    fix : webConfig cors 코드 추가

--- src/main/java/back/tickita/config/WebMvcConfig.java
+++ src/main/java/back/tickita/config/WebMvcConfig.java
@@ -2,8 +2,10 @@ package back.tickita.config;
 
 import back.tickita.interceptor.LoginInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +18,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginInterception);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
