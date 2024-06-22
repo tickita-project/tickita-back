acc92902bc87bb0f7e23ab811df114079a30aea3
lby9906 <lby9906@naver.com>
2024-06-22 13:45:02 +0900

    fix : 그룹 초대 삭제 코드 수정

--- src/main/java/back/tickita/domain/crews/repository/CrewListRepository.java
+++ src/main/java/back/tickita/domain/crews/repository/CrewListRepository.java
@@ -15,18 +15,15 @@ import java.util.Optional;
 @Repository
 public interface CrewListRepository extends JpaRepository<CrewList, Long> {
     List<CrewList> findAllByAccountId(Long accountId);
-    Optional<CrewList> findByAccount_Id(Long accountId);
     Optional<CrewList> findByAccountIdAndCrewsId(Long accountId, Long crewId);
 
     Optional<CrewList> findByAccountAndCrews(Account account, Crews crews);
-    List<CrewList> findAllByCrewsId(Long crewId);
 
     boolean existsByAccountIdAndCrewsId(Long accountId, Long crewId);
 
-    Optional<CrewList> findByCrewsId(Long crewId);
-
-    List<CrewList> findByCrews(Crews crews);
     boolean existsByCrewsIdAndAccountIdAndCrewRole(Long crewId, Long accountId, CrewRole role);
 
     Optional<CrewList> findByAccountIdAndCrewAccept(Long accountId, CrewAccept crewAccept);
+
+    Optional<CrewList> findByAccountIdAndCrewsIdAndCrewAccept(Long accountId, Long crewId, CrewAccept crewAccept);
 }
